5bb8397b3427b68d063fbe1790d94d3ee2f7a8a5
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-03 11:43:55 -0800

    removed the + and - ops

--- firmware/arith_test/arith_test.asm
+++ firmware/arith_test/arith_test.asm
@@ -9,10 +9,10 @@ start:
     DROP        ; Clean up result
 
     ; Test addition 2
-    #$1234      ; Load first test value
-    #$5678      ; Load second test value
-    +         ; Add them together (should be 0x68AC)
-    DROP        ; Clean up result
+    ;#$1234      ; Load first test value
+    ;#$5678      ; Load second test value
+    ;+         ; Add them together (should be 0x68AC)
+    ;DROP        ; Clean up result
 
     ; Test subtraction 1
     #$5000      ; Load first test value
@@ -21,10 +21,10 @@ start:
     DROP        ; Clean up result
 
     ; Test subtraction 2
-    #$5000      ; Load first test value
-    #$2000      ; Load value to subtract
-    -           ; Subtract (should be 0x3000)
-    DROP        ; Clean up result
+    ;#$5000      ; Load first test value
+    ;#$2000      ; Load value to subtract
+    ;-           ; Subtract (should be 0x3000)
+    ;DROP        ; Clean up result
 
     ; Test increment/decrement
     #$0005      ; Load test value
