bab52fa17914f152063af7858ecd592c48278b2c
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-04 18:11:28 -0800

    add drop after each test

--- firmware/comparison_test/comparison_test.asm
+++ firmware/comparison_test/comparison_test.asm
@@ -6,28 +6,34 @@ main:
     #5      ; Push 5
     #5      ; Push another 5
     =       ; Should leave -1 (true)
+    DROP        ; Clean up result
     
     #5
     #6
     =       ; Should leave 0 (false)
+    DROP        ; Clean up result
 
     ; Test signed less than (<)
     #-5     ; Push -5
     #5      ; Push 5
     <       ; Should leave -1 (true)
+    DROP        ; Clean up result
     
     #5
     #-5
     <       ; Should leave 0 (false)
+    DROP        ; Clean up result
 
     ; Test unsigned less than (U<)
     #5
     #10
     U<      ; Should leave -1 (true)
+    DROP        ; Clean up result
     
     #10
     #5
     U<      ; Should leave 0 (false)
+    DROP        ; Clean up result
 
     ; Test with +RET optimization
 ;    : test_eq  ( n1 n2 -- flag )  =+RET ;
