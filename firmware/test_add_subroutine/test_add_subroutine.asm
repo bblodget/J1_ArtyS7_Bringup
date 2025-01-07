53f2d339c7eaab9655877c84183b04eb8de1e544
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-07 08:29:35 -0800

    change comments from ; to //

--- firmware/test_add_subroutine/test_add_subroutine.asm
+++ firmware/test_add_subroutine/test_add_subroutine.asm
@@ -1,19 +1,19 @@
-; Simple test program
-; Pushes two numbers and adds them
-; Uses subroutine calls and return
-; Uses the stack and return stack
-start:                   ; Note the colon after label
-    #$2A #10             ; Push hex 2A (decimal 42) and decimal 10
-    CALL add_nums        ; Call our addition subroutine
-    N[d-1]               ; DROP the result
-    JMP wait_forever     ; Jump to end of program
+// Simple test program
+// Pushes two numbers and adds them
+// Uses subroutine calls and return
+// Uses the stack and return stack
+start:                   // Note the colon after label
+    #$2A #10             // Push hex 2A (decimal 42) and decimal 10
+    CALL add_nums        // Call our addition subroutine
+    N[d-1]               // DROP the result
+    JMP wait_forever     // Jump to end of program
 
-add_nums:                ; Note the colon after label
-     T+N[d-1]            ; Add top two stack items
-     T[T->R,r+1]         ; Save result to return stack (push, don't overwrite)
-     T[r-1]              ; Pop our saved value to T
-     T[RET,r-1]          ; Return to caller
+add_nums:                // Note the colon after label
+     T+N[d-1]            // Add top two stack items
+     T[T->R,r+1]         // Save result to return stack (push, don't overwrite)
+     T[r-1]              // Pop our saved value to T
+     T[RET,r-1]          // Return to caller
 
 wait_forever:
-    T[d+0]               ; NOOP
-    JMP wait_forever     ; Loop forever
+    T[d+0]               // NOOP
+    JMP wait_forever     // Loop forever
