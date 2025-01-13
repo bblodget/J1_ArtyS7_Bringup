dcd28dbfc13ad803e75207fd53bfebd5863b9d48
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-12 17:54:45 -0800

    First version of the stdlib.

---
+++ j1tools/j1tools/assembler/lib/math/math_words.asm
@@ -0,0 +1,11 @@
+// Math operations built from core words
+include "core/core_words.asm"
+
+macro: 2dup ( a b -- a b a b )
+    over
+    over
+;
+
+macro: plus ( a b -- c )
+    T+N[d-1]  // ALU add operation (T+N)
+;
