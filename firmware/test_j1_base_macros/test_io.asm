79cb31c169d3089d764bf9d92a1c9112ee2570ac
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-14 15:41:19 -0800

    improved io test

--- firmware/test_j1_base_macros/test_io.asm
+++ firmware/test_j1_base_macros/test_io.asm
@@ -2,12 +2,26 @@
 include "core/j1_base_macros.asm"
 
 start:
-    // Test I/O operations
-    #1234        // Stack: 1234 (test data)
-    #42          // Stack: 1234 42 (test address)
-    io!          // Stack: empty (write 1234 to IO addr 42)
-    #42          // Stack: 42
-    io@          // Stack: 1234 (read back from IO addr 42)
+    // Test UART write (0x1000)
+    #48          // Stack: 0x48 ('H' in ASCII)
+    #1000        // Stack: 0x48 1000 (UART address)
+    io!          // Stack: empty (write 'H' to UART)
+    
+    // Test UART status read (0x2000)
+    #2000        // Stack: 2000 (MISC_IN_ADDR)
+    io@          // Stack: status (bit 0: !uart_busy, bit 1: uart_valid)
+    
+    // Test UART read (0x1000)
+    #1000        // Stack: status 1000
+    io@          // Stack: status rx_data (if uart_valid was set)
+    
+    // Test ticks counter (0x4000)
+    #4000        // Stack: status rx_data 4000
+    io@          // Stack: status rx_data ticks
+    
+    // Test cycles counter (0x8000)
+    #8000        // Stack: status rx_data ticks 8000
+    io@          // Stack: status rx_data ticks cycles
 
 done:
     noop
