588f4a22e1a4bf388218c48ab1c9ff7d68bd4c7f
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-14 11:12:14 -0800

    Grammar update to handle forth macro names.

--- firmware/test_platform/test_platform.asm
+++ firmware/test_platform/test_platform.asm
@@ -1,5 +1,4 @@
 // Test platform for J1 16KB Dual-Port configuration
-//include "platform/j1_16kb_dualport_macros.asm"
 include "core/j1_base_macros.asm"
 
 // Test program start
@@ -7,44 +6,41 @@ start:
     // Test basic stack operations
     #5 #3        // Push 5 and 3 onto stack
     swap         // Stack: 3 5
-    +            // Stack: 8
-    #10          // Push 10 onto stack
-    u<           //Stack: -1 (true) 8 u< 10
-    drop         // Stack: 
-    //+            // Stack: 8
-    //2dup         // Stack: 5 3 5 3
-    // swap         // Stack: 5 8 3
-    //drop         // Stack: 5 8
-
-    JMP done
+    over         // Stack: 3 5 3
+    nip          // Stack: 3 3
+    dup          // Stack: 3 3 3
+    drop         // Stack: 3 3
+    drop         // Stack: 3
 
     // Test ALU operations
-    //2dup*        // Stack: 5 8 40
-    //2/           // Stack: 5 8 20
-    //2*           // Stack: 5 8 40
-
-    // Test memory operations
-    //#42 #100 !   // Store 42 at address 100
-    //#100 @       // Read from address 100
+    #8           // Stack: 3 8
+    +            // Stack: 11
+    #5           // Stack: 11 5
+    -            // Stack: 6
+    #3           // Stack: 6 3
+    and          // Stack: 2
+    #5           // Stack: 2 5
+    or           // Stack: 7
+    #3           // Stack: 7 3
+    xor          // Stack: 4
+    invert       // Stack: -5 (0xFFFB)
     
-    // Test extended operations
-    //#3 #1 lshift // Shift 3 left by 1 -> 6
-    //#8 #2 rshift // Shift 8 right by 2 -> 2
+    // Test comparisons
+    #10 #5       // Stack: -5 10 5
+    u<           // Stack: -5 0 (false: 10 is not < 5)
+    drop         // Stack: -5
+    #3 #8        // Stack: -5 3 8
+    <            // Stack: -5 1 (true: 3 < 8)
+    drop         // Stack: -5
+    #4 #4        // Stack: -5 4 4
+    =            // Stack: -5 1 (true: 4 = 4)
+    drop         // Stack: -5
 
-    // Test return stack
-    //>r           // Move value to return stack
-    //r@           // Copy from return stack
-    //r>           // Restore from return stack
-
-    // Test increment/decrement
-    //#10
-    //dup1+        // Stack: 10 11
-    //1-           // Stack: 10 10
-
-    // Test optimized memory operations
-    //#200 dup@    // Read and keep address
-    //tuck!        // Store and keep value
+    // Test shifts
+    #4           // Stack: -5 4
+    2*           // Stack: -5 8
+    2/           // Stack: -5 4
 
 done:
-    //noop
+    noop
     JMP done     // Loop forever
--- j1tools/j1tools/assembler/lib/core/j1_base_macros.asm
+++ j1tools/j1tools/assembler/lib/core/j1_base_macros.asm
@@ -19,18 +19,18 @@ macro: invert  ( a -- ~a )         ~T ;
 macro: =       ( a b -- f )        N==T[d-1] ;
 macro: <       ( a b -- f )        N<T[d-1] ;
 macro: u<      ( a b -- f )        Nu<T[d-1] ;
-//macro: 2/      ( n -- n/2 )        T2/ ;
-//macro: 2*      ( n -- n*2 )        T2* ;
+macro: 2/      ( n -- n/2 )        T2/ ;
+macro: 2*      ( n -- n*2 )        T2* ;
 
 // Return stack operations
-//macro: >r      ( a -- R: -- a )    N[T->R,r+1,d-1] ;
-//macro: r>      ( -- a R: a -- )    rT[T->N,r-1,d+1] ;
-//macro: r@      ( -- a R: a -- a )  rT[T->N,d+1] ;
+macro: >r      ( a -- R: -- a )    N[T->R,r+1,d-1] ;
+macro: r>      ( -- a R: a -- )    rT[T->N,r-1,d+1] ;
+macro: r@      ( -- a R: a -- a )  rT[T->N,d+1] ;
 macro: rdrop   ( -- R: a -- )      T[r-1] ;
 
 // I/O operations
-//macro: io@     ( addr -- val )     io[T][IORD] ;
-//macro: io!     ( val addr -- )     T[N->io[T],d-1] ;
+macro: io@     ( addr -- val )     io[T][IORD] ;
+macro: io!     ( val addr -- )     T[N->io[T],d-1] ;
 
 // Status and control
 macro: depth   ( -- n )            status[T->N,d+1] ;
@@ -40,18 +40,18 @@ macro: eint    ( -- )              T[fEINT] ;
 
 // Basic elided words
 macro: 2dupand ( a b -- a b a&b )  T&N[T->N,d+1] ;
-//macro: 2dup<   ( a b -- a b f )    N<T[T->N,d+1] ;
-//macro: 2dup=   ( a b -- a b f )    N==T[T->N,d+1] ;
+macro: 2dup<   ( a b -- a b f )    N<T[T->N,d+1] ;
+macro: 2dup=   ( a b -- a b f )    N==T[T->N,d+1] ;
 macro: 2dupor  ( a b -- a b a|b )  T|N[T->N,d+1] ;
-//macro: 2dup+   ( a b -- a b sum )  T+N[T->N,d+1] ;
-//macro: 2dup-   ( a b -- a b dif )  N-T[T->N,d+1] ;
-//macro: 2dupu<  ( a b -- a b f )    Nu<T[T->N,d+1] ;
+macro: 2dup+   ( a b -- a b sum )  T+N[T->N,d+1] ;
+macro: 2dup-   ( a b -- a b dif )  N-T[T->N,d+1] ;
+macro: 2dupu<  ( a b -- a b f )    Nu<T[T->N,d+1] ;
 macro: 2dupxor ( a b -- a b x )    T^N[T->N,d+1] ;
-//macro: dup>r   ( a -- a R: -- a )  T[T->R,r+1] ;
+macro: dup>r   ( a -- a R: -- a )  T[T->R,r+1] ;
 macro: overand ( a b -- a a&b )    T&N ;
-//macro: over>   ( a b -- a b>a )    N<T ;
-//macro: over=   ( a b -- a b=a )    N==T ;
+macro: over>   ( a b -- a b>a )    N<T ;
+macro: over=   ( a b -- a b=a )    N==T ;
 macro: overor  ( a b -- a a|b )    T|N ;
-//macro: over+   ( a b -- a a+b )    T+N ;
-//macro: overu>  ( a b -- a b>ua )   Nu<T ;
+macro: over+   ( a b -- a a+b )    T+N ;
+macro: overu>  ( a b -- a b>ua )   Nu<T ;
 macro: overxor ( a b -- a a^b )    T^N ;
