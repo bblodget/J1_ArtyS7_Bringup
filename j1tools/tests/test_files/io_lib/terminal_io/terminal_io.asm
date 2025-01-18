4bc4ff27c928f29a7f697bb5dff453001f8dc819
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-18 14:37:30 -0800

    output Hi!

--- j1tools/tests/test_files/io_lib/terminal_io/terminal_io.asm
+++ j1tools/tests/test_files/io_lib/terminal_io/terminal_io.asm
@@ -3,9 +3,9 @@ include "io/terminal_io.asm"
 
 start:                  // Program entry point
     #$48 CALL emit     // Send 'H' (ASCII 0x48)
-    // #$69 CALL emit  // Send 'i' (ASCII 0x69)
-    // #$21 CALL emit  // Send '!' (ASCII 0x21)
-    // #$0A CALL emit  // Send newline (ASCII 0x0A)
+    #$69 CALL emit  // Send 'i' (ASCII 0x69)
+    #$21 CALL emit  // Send '!' (ASCII 0x21)
+    #$0A CALL emit  // Send newline (ASCII 0x0A)
 
     // Test receive functionality
     // CALL key?       // Check if character available
@@ -18,6 +18,6 @@ start:                  // Program entry point
 //     // #$4F #$4B CALL 2emit  // Send "OK" (ASCII 0x4F, 0x4B)
 //     // #$0A CALL emit        // Send newline
 
-// wait_forever:
-//     noop
-//     JMP wait_forever
+wait_forever:
+     noop
+     JMP wait_forever
