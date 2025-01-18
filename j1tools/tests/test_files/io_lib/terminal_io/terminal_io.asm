220a35be7d96640860274f1635506ca006518d23
Brandon Blodget <brandon.blodget@gmail.com>
2025-01-18 12:44:06 -0800

    cleaned up files

--- j1tools/tests/test_files/io_lib/terminal_io/terminal_io.asm
+++ j1tools/tests/test_files/io_lib/terminal_io/terminal_io.asm
@@ -1,18 +1,17 @@
-// Test terminal I/O library functionality
+
 include "io/terminal_io.asm"
 
-start:
-    // Test transmit functionality
-    #$48 CALL emit      // Send 'H' (ASCII 0x48)
-    // #$69 CALL emit      // Send 'i' (ASCII 0x69)
-    // #$21 CALL emit      // Send '!' (ASCII 0x21)
-    // #$0A CALL emit      // Send newline (ASCII 0x0A)
+start:                  // Program entry point
+    #$48 CALL emit     // Send 'H' (ASCII 0x48)
+    // #$69 CALL emit  // Send 'i' (ASCII 0x69)
+    // #$21 CALL emit  // Send '!' (ASCII 0x21)
+    // #$0A CALL emit  // Send newline (ASCII 0x0A)
 
     // Test receive functionality
-    // CALL key?           // Check if character available
-    // ZJMP no_input      // Skip if no input
-    // CALL key           // Read the character
-    // CALL emit          // Echo it back
+    // CALL key?       // Check if character available
+    // ZJMP no_input   // Skip if no input
+    // CALL key        // Read the character
+    // CALL emit       // Echo it back
 
 // no_input:
 //     // Test 2emit for sending two characters
