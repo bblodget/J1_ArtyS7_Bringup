aa4f70758dffd7ae1cc4e019017a17ac44f2c702
Brandon Blodget <brandon.blodget@gmail.com>
2025-03-07 07:36:20 -0800

    updated io_lib labels

--- j1tools/tests/test_files/io_lib/io_words/io_words.asm
+++ j1tools/tests/test_files/io_lib/io_words/io_words.asm
@@ -4,29 +4,12 @@ JMP start
 include "core/j1_base_macros.asm"
 include "io/terminal_io.asm"
 
-start:
+: start
     key        // Read the character
     emit       // Echo it back
     #$0A emit  // Send newline (ASCII 0x0A)
 
-// start:                  // Program entry point
-//     #$48 CALL emit     // Send 'H' (ASCII 0x48)
-//     #$69 CALL emit  // Send 'i' (ASCII 0x69)
-//     #$21 CALL emit  // Send '!' (ASCII 0x21)
-//     #$0A CALL emit  // Send newline (ASCII 0x0A)
-
-    // Test receive functionality
-// read_key:
-//     CALL key        // Read the character
-//     CALL emit       // Echo it back
-//     #$0A CALL emit  // Send newline (ASCII 0x0A)
-
-// send_ok:
-//     // Test 2emit for sending two characters
-//     #$4F #$4B CALL 2emit  // Send "OK" (ASCII 0x4F, 0x4B)
-//     #$0A CALL emit        // Send newline
-
-wait_forever:
+: wait_forever
      noop
      JMP wait_forever
 
