0263788962b018b4d4a0e5ed202d1f6ee8af8e95
Brandon Blodget <brandon.blodget@gmail.com>
2025-03-07 07:14:34 -0800

    update basic_include test

--- j1tools/tests/test_files/include/basic_include/basic_include.asm
+++ j1tools/tests/test_files/include/basic_include/basic_include.asm
@@ -1,11 +1,15 @@
+ORG #$0000
+JMP start
+
 include "words.asm"
 
-start:                         // Note the colon after label
-    #42 #10
-    dup
-    drop
-    JMP wait_forever           // Jump to end of program
+: start
+    // Push two numbers and add them
+    #$2A #10             // Push hex 2A (decimal 42) and decimal 10
+    CALL add_nums        // Call our addition subroutine
+    N[d-1]               // DROP the result
+    JMP wait_forever     // Jump to end of program
 
-wait_forever:
-    noop            
-    JMP wait_forever           // Loop forever
+: wait_forever
+     noop
+     JMP wait_forever
--- j1tools/tests/test_files/include/basic_include/words.asm
+++ j1tools/tests/test_files/include/basic_include/words.asm
@@ -1,6 +1,12 @@
 // Macro's for basic forth words
 
-macro: dup ( a -- a a ) T[T->N,d+1] ;
-macro: drop ( a -- ) N[d-1] ;
-macro: noop ( -- ) T[d+0] ;
+macro: dup ( a -- a a ) T[T->N,d+1] endmacro
+macro: drop ( a -- ) N[d-1]         endmacro
+macro: noop ( -- ) T[d+0]           endmacro
+
+: add_nums                // Note the colon after label
+    T+N[d-1]             // Add top two stack items
+    T[T->R,r+1]          // Save result to return stack (push, don't overwrite)
+    T[r-1]               // Pop our saved value to T
+    T[RET,r-1]           // Return to caller
 
