de5e43b8ab965a9e38f5213afc880dfff5947625
Brandon Blodget <brandon.blodget@gmail.com>
2025-03-22 08:36:05 -0700

    use the drop macro

--- j1tools/tests/test_files/include/basic_include/basic_include.asm
+++ j1tools/tests/test_files/include/basic_include/basic_include.asm
@@ -7,7 +7,7 @@ include "words.asm"
     // Push two numbers and add them
     $2A 10             // Push hex 2A (decimal 42) and decimal 10
     CALL 'add_nums        // Call our addition subroutine
-    N[d-1]               // DROP the result
+    drop               // DROP the result
     JMP 'wait_forever     // Jump to end of program
 
 : wait_forever
