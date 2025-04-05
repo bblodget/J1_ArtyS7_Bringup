0a8531940ef86dce8d477b77cf2d8d960678e15b
Brandon Blodget <brandon.blodget@gmail.com>
2025-04-05 16:55:42 -0700

    change start to main

--- j1tools/tests/test_files/directives/arch_flag/arch_flag.asm
+++ j1tools/tests/test_files/directives/arch_flag/arch_flag.asm
@@ -1,14 +1,15 @@
-// Minimal Architecture Flag Test
+// Architecture Flag Test File
+// This file tests the .arch_flag directive
 
 // Set architecture flags
-.arch_flag fetch_type FETCH_TYPE_DUALPORT // Use dual-port memory (ARCH_FETCH_TYPE = 1)
-.arch_flag alu_ops ALU_OPS_ORIGINAL  // Use original ALU operations (ARCH_ALU_OPS = 0)
+.arch_flag fetch_type FETCH_TYPE_DUALPORT    // Use dual-port memory (ARCH_FETCH_TYPE = 1)
+.arch_flag alu_ops ALU_OPS_ORIGINAL         // Use original ALU operations (ARCH_ALU_OPS = 0)
 
-// Simple test program that does nothing but jump to itself
-: start
+// Simple test program
+: main
     42     // Push 42
     N       // Drop the value (equivalent to 'drop')
     
     // Loop forever
-    JMP 'start
+    JMP 'main
 
