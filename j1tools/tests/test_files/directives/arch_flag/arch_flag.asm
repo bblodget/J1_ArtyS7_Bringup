4e60b3b6597ce7e4a891b1f49ebed63997234ac2
Brandon Blodget <brandon.blodget@gmail.com>
2025-04-05 16:52:54 -0700

    Enhance J1 assembler to support architecture flags in program structure

--- j1tools/tests/test_files/directives/arch_flag/arch_flag.asm
+++ j1tools/tests/test_files/directives/arch_flag/arch_flag.asm
@@ -1,8 +1,8 @@
 // Minimal Architecture Flag Test
 
 // Set architecture flags
-.arch_flag fetch_type dualport    // Use dual-port memory (ARCH_FETCH_TYPE = 1)
-.arch_flag alu_ops original       // Use original ALU operations (ARCH_ALU_OPS = 0)
+.arch_flag fetch_type FETCH_TYPE_DUALPORT // Use dual-port memory (ARCH_FETCH_TYPE = 1)
+.arch_flag alu_ops ALU_OPS_ORIGINAL  // Use original ALU operations (ARCH_ALU_OPS = 0)
 
 // Simple test program that does nothing but jump to itself
 : start
